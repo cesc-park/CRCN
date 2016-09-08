e75a58c9d51f2412fe3c22e5e67c36807148db76
Cesc Chunseong Park <cs.park@vision.snu.ac.kr>
2016-09-08 23:59:46 +0900

    Remove unnecessary modules

--- crcn_training.py
+++ crcn_training.py
@@ -2,7 +2,6 @@ import sys
 sys.path.append("./keras")
 sys.path.append("./entity")
 import theano
-from keras.layers.recurrent import CRCN,RCN
 import numpy as np
 import pickle
 from gensim import models
